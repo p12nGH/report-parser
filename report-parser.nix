280a882447b1ceb101e1f226033cae50c733d9cb
Gleb Gagarin <gleb@sifive.com>
2019-04-10 22:39:50 -0700

    Added .nix file (generated with cabal2nix)

---
+++ report-parser.nix
@@ -0,0 +1,12 @@
+{ mkDerivation, attoparsec, base, bytestring, stdenv }:
+mkDerivation {
+  pname = "report-parser";
+  version = "0.1.0.1";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [ attoparsec base bytestring ];
+  executableHaskellDepends = [ attoparsec base bytestring ];
+  description = "Parser combinator library for parsing log files and text reports";
+  license = stdenv.lib.licenses.bsd3;
+}
